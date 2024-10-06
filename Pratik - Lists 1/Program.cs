aba254bb3607be42b7f5e230542b62ee2f19ad89
Aslıhan ESEN <aslyqc14@gmail.com>
2024-10-07 00:24:34 +0300

    Proje dosyası ekle.

---
+++ Pratik - Lists 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Ziyaretçilerin isimlerini içeren liste
+        List<string> davetliler = new List<string>()
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        // İsimleri tek tek ekrana yazdırmak için foreach döngüsü
+        foreach (string davetli in davetliler)
+        {
+            Console.WriteLine(davetli);
+        }
+    }
+}
+
